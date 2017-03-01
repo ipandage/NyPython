232c79e78fdeb5d5e1e276c4864e4848f7c9246b
Nyloner <nyloner.root@gmail.com>
2017-03-01 23:15:07 +0800

    add cpu_draw

--- proc/app.py
+++ proc/app.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QColor,QPen,QFont
 from proc import *
 import time
+import random
 
 class CPU(QtCore.QThread):
     _cpu_infor_signal=QtCore.pyqtSignal(dict)
@@ -11,6 +12,11 @@ class CPU(QtCore.QThread):
 
     def run(self):
         for result in get_cpu_infor():
+            '''
+            result={}
+            for key in ['cpu0','cpu1','cpu2','cpu3']:
+                result[key]=random.randint(0,20)/100
+            '''
             self._cpu_infor_signal.emit(result)
             time.sleep(1)
 
@@ -46,11 +52,39 @@ class Network(QtCore.QThread):
             self._net_infor_signal.emit(infor)
             time.sleep(1)
 
+class DrawMemory(QtWidgets.QWidget):
+    def __init__(self,mem_infor):
+        super(DrawMemory,self).__init__()
+
+    def paintEvent(self,event):
+        painter=QtGui.QPainter()
+        painter.begin(self)
+        self.draw_mem(painter)
+        painter.end()
+
+    def draw_mem(self,painter):
+        pass
+
+class DrawNetwork(QtWidgets.QWidget):
+    def __init__(self,network_data):
+        super(DrawNetwork,self).__init__()
+        self.network_data=network_data
+
+    def paintEvent(self,event):
+        painter=QtGui.QPainter()
+        painter.begin(self)
+        self.draw_network(painter)
+        painter.end()
+
+    def draw_network(self,painter):
+        pass
+
+
 class DrawCPU(QtWidgets.QWidget):
     def __init__(self,cpu_data):
         super(DrawCPU,self).__init__()
         self.cpu_data=cpu_data
-        self.colors=[QColor(255,160,90),QColor(255,100,90),QColor(200,200,200),QColor(100,100,100)]
+        self.colors=[QColor(255,160,90),QColor(255,100,90),QColor(200,100,200),QColor(100,100,100)]
 
     def paintEvent(self,event):
         painter=QtGui.QPainter()
@@ -61,18 +95,43 @@ class DrawCPU(QtWidgets.QWidget):
     def draw_cpus(self,painter):
         cpus=self.cpu_data[0]
         cpus=sorted(cpus.items(),key=lambda x:x[0])
+        length=len(cpus)
+        width=int(750/length)
         text_font=QFont()
-        text_font.setPointSize(20)
+        text_font.setPointSize(20-2*length)
         text_font.setItalic(True)
-        for i in range(len(cpus)-1):
+        line_colors={}
+        for i in range(length):
             painter.setPen(QPen(QColor(100,120,30),2))
             painter.setBrush(self.colors[i]);
-            painter.drawRect(20,i*80+20,100,60)
+            if cpus[i][0] not in line_colors:
+                line_colors[cpus[i][0]]=self.colors[i]
+            painter.drawRect(i*(width)+30,400,50,30)
             painter.setPen(QColor(110,50,50))
             painter.setFont(text_font)
-            str_text="{0} : {1:.2%}".format(cpus[i+1][0],cpus[i+1][1])
-            painter.drawText(130,i*80+60,str_text)
-
+            str_text="{0} : {1:.2%}".format(cpus[i][0],cpus[i][1])
+            painter.drawText(i*(width)+30,450,str_text)
+        painter.setPen(QColor(50,50,50))
+        painter.drawLine(30,30,30,330)
+        painter.drawLine(30,330,630,330)
+        data_length=len(self.cpu_data[:60])
+        for i in range(data_length):
+            if i==data_length-1:
+                break
+            for cpu in self.cpu_data[i]:
+                if cpu=='cpu':
+                    continue
+                color=line_colors[cpu]
+                painter.setPen(QPen(color,2))
+                try:
+                    y1=330-self.cpu_data[i][cpu]*300
+                except:
+                    y1=320
+                try:
+                    y2=330-self.cpu_data[i+1][cpu]*300
+                except:
+                    y2=320
+                painter.drawLine(i*10+30,y1,i*10+40,y2)
 
 class Manager(Ui_MainWindow,QtWidgets.QMainWindow):
     def __init__(self):
